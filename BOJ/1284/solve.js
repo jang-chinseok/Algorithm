a1bb4485b6bdff9b0df62c8c30bda20e2ff42677
jang-chinseok <72973917+jang-chinseok@users.noreply.github.com>
2021-06-20 08:18:39 +0900

    solving with node.js
    testing for solveing node.js

---
+++ BOJ/1284/solve.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl =readline.createInterface({
+   input : process.stdin,
+   output: process.stdout
+});
+
+let imput =[];
+
+rl.on('line',function(line){
+    input = line.split(' ').map((el)=> parseInt(el));
+    }).on('close',function(){
+       
+        let num1 = Number(input[0]);
+        let num2 = Number(input[1]);
+
+
+        
+      //이 아래가 프로그램을 작성하는 곳 이라고 생각하면 된다.
+        console.log(num1+num2);
+
+
+        
+        process.exit();
+        });
