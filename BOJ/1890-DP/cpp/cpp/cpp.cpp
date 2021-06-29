24aa55191b504af7c36a20c0662266544d7cd0ba
jang-chinseok <72973917+jang-chinseok@users.noreply.github.com>
2021-06-29 23:03:41 +0900

    soving

---
+++ BOJ/1890-DP/cpp/cpp/cpp.cpp
@@ -0,0 +1,37 @@
+﻿// cpp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+int dfs(int,int);
+int N,count_of_root=0;
+int numy,numx;
+
+int chackLink
+int main()
+{   
+    string s;
+    int map[101][101];
+    int memory[101][101];
+
+    cin>>N;
+    for (int i = 0; i < s.size(); i++) {
+        cin >> s;
+        map[i][] = s;
+    }
+
+
+    dfs(0,0);
+    
+
+    cout << count_of_root;
+
+}
+
+int dfs(int numy,int numx){
+    for()
+
+
+    return 0;
+}
+
